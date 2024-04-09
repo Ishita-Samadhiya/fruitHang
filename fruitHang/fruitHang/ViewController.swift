e5498e27b9dcd1bea81ebd0d6e9e2580da1ed78e
Ishita-Samadhiya <i.samadhiya@my.metroed.net>
2024-04-09 14:39:50 -0700

    finale

--- fruitHang/fruitHang/ViewController.swift
+++ fruitHang/fruitHang/ViewController.swift
@@ -8,20 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentGame: Game!
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        fruitImageView.image = UIImage(named: "fruit\(currentGame.incorrectMovesRemaining+1)")
+    }
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord,
+            incorrectMovesRemaining: incorrectMovesAllowed,
+            guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+    func enableLetterButtons(_ enable: Bool) {
+      for button in letterButtons {
+        button.isEnabled = enable
+      }
+    }
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    func updateGameState() {
+      if currentGame.incorrectMovesRemaining == 0 {
+        totalLosses += 1
+      } else if currentGame.word == currentGame.formattedWord {
+        totalWins += 1
+      } else {
+        updateUI()
+      }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
     }
-    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+    var listOfWords = ["berry", "fruit", "strawberry", "mango", "banana", "pear"]
     @IBOutlet var fruitImageView: UIImageView!
     @IBOutlet var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
     @IBOutlet var correctWordLabel: UILabel!
     let incorrectMovesAllowed = 6
-    var totalWins = 0
-    var totalLosses = 0
-    @IBAction func letterButtonPressed(_ sender: UIButton) {
-        sender.isEnabled = false
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
     }
-    
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
 }
 
